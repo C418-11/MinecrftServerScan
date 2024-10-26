ff0d0f81324bfe344965f25acb526ecce5fba9f3
C418____11 <553515788@qq.com>
2024-10-26 17:29:04 +0800

    打包支持 再也不手贱改bat的路径\为/了

--- build_tools/CopyFiles.bat
+++ build_tools/CopyFiles.bat
@@ -2,18 +2,18 @@
 REM -*- coding: utf-8 -*-
 REM 将构建好的必要文件复制到输出目录
 
-IF NOT EXIST "../dist/ServerScanner" (
+IF NOT EXIST "..\dist\ServerScanner" (
   ECHO Output dir not found.
   EXIT 9009
 )
-COPY /V "DebugConsole.bat" "../dist/ServerScanner"
+COPY /V "DebugConsole.bat" "..\dist\ServerScanner"
 IF %ERRORLEVEL% NEQ 0 (
   ECHO Warning: Failed copy debug script...
 )
 
 TITLE Copy pyd files
 
-START /WAIT /B CMD /C "CALL RecursiveCopy/Copy Build/pyd ../dist/ServerScanner/_internal"
+START /WAIT /B CMD /C "CALL RecursiveCopy\Copy Build\pyd ..\dist\ServerScanner\_internal"
 IF %ERRORLEVEL% NEQ 0 (
   ECHO "Error during copy pyd files... (%ERRORLEVEL%)"
   EXIT %ERRORLEVEL%
@@ -21,14 +21,14 @@ IF %ERRORLEVEL% NEQ 0 (
 
 TITLE Copy Textures
 
-MKDIR "../dist/ServerScanner/Textures"
+MKDIR "..\dist\ServerScanner\Textures"
 IF %ERRORLEVEL% NEQ 0 (
   IF %ERRORLEVEL% NEQ 1 (
-    ECHO Failed build path '../dist/ServerScanner/Textures'...
+    ECHO Failed build path '..\dist\ServerScanner\Textures'...
     EXIT %ERRORLEVEL%
   )
 )
-START /WAIT /B CMD /C "CALL RecursiveCopy/Copy ../Textures ../dist/ServerScanner/Textures"
+START /WAIT /B CMD /C "CALL RecursiveCopy\Copy ..\Textures ..\dist\ServerScanner\Textures"
 IF %ERRORLEVEL% NEQ 0 (
   ECHO "Error during copy textures... (%ERRORLEVEL%)"
   EXIT %ERRORLEVEL%
--- build_tools/setup.py
+++ build_tools/setup.py
@@ -129,10 +129,10 @@ def _main():
         compiler.make_all(fr"{base_path}\MinecraftServerScanner")
 
         compiler.make(fr"{stdlib_path}\uuid.py")
-        # compiler.make(fr"{lib_path}\__future__.py")
+        compiler.make(fr"{stdlib_path}\__future__.py")  # PIL依赖
         compiler.make_all(fr"{stdlib_path}\concurrent\futures")
         compiler.make_all(fr"{lib_path}\func_timeout")
-        compiler.make_all(fr"{lib_path}\json")
+        compiler.make_all(fr"{stdlib_path}\json")
         shutil.copytree(
             fr"{lib_path}\PIL",
             os.path.join(compiler.pyd_path, "PIL"),
